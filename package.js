a9d558533b0db75f6b86c44719ba59f34af18b09
Max Nowack <max@unsou.de>
2014-11-28 17:00:39 +0100

    changed package description

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   summary: "Reactive Base class for building objects backed by Collection data",
   version: '1.2.0',
-  name: "mrt:reactive-class",
-  githubUrl: 'https://github.com/lingz/meteor-reactive-class',
+  name: "maxnowack:reactive-class",
+  githubUrl: 'https://github.com/maxnowack/meteor-reactive-class',
 });
 
 Package.on_use(function(api) {
